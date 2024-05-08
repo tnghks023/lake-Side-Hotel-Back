ef6679da912cfef0fd7eae5b52562f9a4b659d68
shwan <tnghks023@naver.com>
2024-05-09 01:49:19 +0900

    FRONT_URL modify

--- src/main/java/com/shwan/lakeSidehotel/security/CorsConfig.java
+++ src/main/java/com/shwan/lakeSidehotel/security/CorsConfig.java
@@ -27,6 +27,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+//        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin(FRONT_URL);
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
